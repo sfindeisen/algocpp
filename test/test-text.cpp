eb6af8abea8dbfab830345ee751ff37af4eeaaf8
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2020-08-31 20:08:24 +0200

    implement KMP algorithm: init P array (more test cases)

--- test/test-text.cpp
+++ test/test-text.cpp
@@ -13,4 +13,52 @@ BOOST_AUTO_TEST_CASE(test_makeP) {
     std::vector<int> p(kmp::makeP(""));
     BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
   }
+
+  {
+    std::vector<size_t> expected_p {0,0};
+    std::vector<int> p(kmp::makeP("a"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,0};
+    std::vector<int> p(kmp::makeP("ab"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,1};
+    std::vector<int> p(kmp::makeP("aa"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,1,0};
+    std::vector<int> p(kmp::makeP("aab"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,0,0};
+    std::vector<int> p(kmp::makeP("abb"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,0,1};
+    std::vector<int> p(kmp::makeP("aba"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,0,1,2,3};
+    std::vector<int> p(kmp::makeP("ababa"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
+
+  {
+    std::vector<size_t> expected_p {0,0,1,2,3,4};
+    std::vector<int> p(kmp::makeP("aaaaa"));
+    BOOST_CHECK_EQUAL_COLLECTIONS(p.begin(), p.end(), expected_p.begin(), expected_p.end());
+  }
 }
